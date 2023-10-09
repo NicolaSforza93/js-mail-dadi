0337fbf78e971faa1666c643088cc9e126fa19b3
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-10-09 17:25:15 +0200

    fix console log

--- js/app.js
+++ js/app.js
@@ -3,29 +3,29 @@ let userEmail;
 userEmail = prompt('Inserisci la tua Email');
 // console.log(userEmail)
 // - dichiaro un array che contiene la lista di email
-const emailList = ['giacomo@gmail.com', 'francesco@gmail.com', 'gianluca@gmail.com', 'pina@gmail.com', 'nicola@hotmail.it'];
+const emailsList = ['giacomo@gmail.com', 'francesco@gmail.com', 'gianluca@gmail.com', 'pina@gmail.com', 'nicola@hotmail.it'];
 // console.log(emailList)
 
 let emailFound = false;
 
 // - controllo tutti gli elementi dell'array 
-for (let i = 0; i < emailList.length; i++) {
+for (let i = 0; i < emailsList.length; i++) {
     // console.log(emailList[i])
 //   - recupero l'elemento corrente 
-    const currentEmail = emailList[i];
+    const currentEmail = emailsList[i];
     
 //   - se l'elemento è uguale all'email inserita
     if (userEmail === currentEmail) {
         emailFound = true;
 //     - stampo email trovata
-        console.log('trovata email');
+        // console.log('trovata email');
     } else {
 //   - altrimenti stampo email non trovata
-        console.log('email non trovata');
+        // console.log('email non trovata');
     }
 }
 
-if (emailFound == true) {
+if (emailFound === true) {
     console.log('sei in lista');
 } else {
     console.log('non puoi accedere');
