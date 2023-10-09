c3d0d162abe70259a01cb6e215b33c395494579b
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-10-09 16:47:04 +0200

    added email and file readme

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// - chiedo all'utente la sua email
+let userEmail;
+userEmail = prompt('Inserisci la tua Email');
+// console.log(userEmail)
+// - dichiaro un array che contiene la lista di email
+const emailList = ['giacomo@gmail.com', 'francesco@gmail.com', 'gianluca@gmail.com', 'pina@gmail.com', 'nicola@hotmail.it'];
+// console.log(emailList)
+
+let emailFound = false;
+
+// - controllo tutti gli elementi dell'array 
+for (let i = 0; i < emailList.length; i++) {
+    // console.log(emailList[i])
+//   - recupero l'elemento corrente 
+    const currentEmail = emailList[i];
+    
+//   - se l'elemento è uguale all'email inserita
+    if (userEmail === currentEmail) {
+        emailFound = true;
+//     - stampo email trovata
+        console.log('trovata email');
+    } else {
+//   - altrimenti stampo email non trovata
+        console.log('email non trovata');
+    }
+}
+
+if (emailFound == true) {
+    console.log('sei in lista');
+} else {
+    console.log('non puoi accedere');
+}
